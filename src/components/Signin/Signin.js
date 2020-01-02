35cbfc3c6b8736d0b2850d36d0f8099720efdd99
EJ-C <10ngweekendofficial@gmail.com>
2020-01-02 04:13:21 -0500

    deploy

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/signin', {
+    fetch('https://enigmatic-gorge-53717.herokuapp.com', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
