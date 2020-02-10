9c67c4cf04cb9fdb2f41386a3dd6f00356aa2b48
EJ-C <10ngweekendofficial@gmail.com>
2020-02-10 14:16:16 -0500

    add info signin page

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -43,6 +43,8 @@ class Signin extends React.Component {
           <div className="measure">
             <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
               <legend className="f1 fw6 ph0 mh0">Sign In</legend>
+              <div>Using ID: test@test.com</div>
+              <a>Password: test</a>
               <div className="mt3">
                 <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
                 <input
